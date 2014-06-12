433d51daf59e9c77908983e11231df61f6c7c1bc
Brandon Hansen <bh@jesusculture.com>
2014-06-11 20:59:36 -0700

    Filter channels

--- public/js/src/controllers.js
+++ public/js/src/controllers.js
@@ -1,12 +1,14 @@
 'use strict';
 
-app.controller('VideoController', ['$scope', '$timeout', 'VineService', 
-  function($scope, $timeout, VineService){
+app.controller('VideoController', ['$scope', '$timeout', 'VineSearch', 'VinePopular', 'VineRecent', 'VineChannels', 
+  function($scope, $timeout, VineSearch, VinePopular, VineRecent, VineChannels){
 
     $scope.videos = [];
     $scope.loading = false;
     $scope.searchTerm = "fail";
     $scope.page = 1;
+    $scope.categories = [];
+    $scope.category = null;
     var doSearchTimeout = false;
 
     $scope.searchVids = function(){
@@ -19,11 +21,27 @@ app.controller('VideoController', ['$scope', '$timeout', 'VineService',
       {
         $scope.loading = true;
 
-        VineService.get({ tag: $scope.searchTerm, page: $scope.page }, function(response){
-          $scope.videos.push.apply($scope.videos, response.data.records);
-          $scope.loading = false;
-        });
+        if($scope.category){
+          VineRecent.get({ tag: $scope.category.featuredChannelId, page: $scope.page }, function(response){
+            $scope.videos.push.apply($scope.videos, response.data.records);
+            $scope.loading = false;
+            console.log(response.data.records);
+          });
+        }
+        else
+        {
+          VineSearch.get({ tag: $scope.searchTerm, page: $scope.page }, function(response){
+            $scope.videos.push.apply($scope.videos, response.data.records);
+            $scope.loading = false;
+          });
+        }
       }
+    };
+
+    $scope.loadCategories = function(){
+      VineChannels.get({}, function(response){
+        $scope.categories = response.data.records;
+      });
     };
 
     $scope.loadMore = function(){
@@ -52,6 +70,17 @@ app.controller('VideoController', ['$scope', '$timeout', 'VineService',
     };
 
     $scope.$watch('searchTerm', function(){
+      if(doSearchTimeout){
+        $timeout.cancel(doSearchTimeout);
+      }
+
+      doSearchTimeout = $timeout(function(){
+        $scope.videos = [];
+        $scope.searchVids();
+      }, 250);
+    });
+
+    $scope.$watch('category', function(){
       if(doSearchTimeout){
         $timeout.cancel(doSearchTimeout);
       }
--- public/js/src/services.js
+++ public/js/src/services.js
@@ -1,7 +1,46 @@
 'use strict';
 
-app.factory('VineService', ['$resource', function($resource){
+app.factory('VineSearch', ['$resource', function($resource){
   return $resource('http://bhvine.herokuapp.com/search/:tag', 
+  {
+    tag: 'search',
+    callback: 'JSON_CALLBACK'
+  }, 
+  {
+    get: {
+      method: 'JSONP'
+    }
+  });
+}]);
+
+app.factory('VinePopular', ['$resource', function($resource){
+  return $resource('http://bhvine.herokuapp.com/popular', 
+  {
+    tag: 'search',
+    callback: 'JSON_CALLBACK'
+  }, 
+  {
+    get: {
+      method: 'JSONP'
+    }
+  });
+}]);
+
+app.factory('VineRecent', ['$resource', function($resource){
+  return $resource('http://bhvine.herokuapp.com/recent/:tag', 
+  {
+    tag: 'search',
+    callback: 'JSON_CALLBACK'
+  }, 
+  {
+    get: {
+      method: 'JSONP'
+    }
+  });
+}]);
+
+app.factory('VineChannels', ['$resource', function($resource){
+  return $resource('http://bhvine.herokuapp.com/channels', 
   {
     tag: 'search',
     callback: 'JSON_CALLBACK'
