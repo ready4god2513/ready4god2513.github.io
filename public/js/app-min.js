ea16828767c53f884bb40d495e8e17afa130aacc
Brandon Hansen <bh@jesusculture.com>
2014-06-18 23:19:41 -0700

    Cleaner layout

--- public/js/app-min.js
+++ public/js/app-min.js
@@ -1829,11 +1829,22 @@ app.controller("AmazonController", [ "$scope", "$timeout", "Amazon", function($s
     $scope.search = "Ralph";
     $scope.category = "Toys";
     $scope.products = [];
+    $scope.loading = false;
+    var doSearchTimeout = false;
     $scope.searchAmazon = function() {
         Amazon.search($scope.category, $scope.search, function(data) {
             $scope.products = data.Item;
         });
     };
+    $scope.$watch("search", function() {
+        if (doSearchTimeout) {
+            $timeout.cancel(doSearchTimeout);
+        }
+        $scope.page = 1;
+        doSearchTimeout = $timeout(function() {
+            $scope.searchAmazon();
+        }, 500);
+    });
 } ]);
 
 "use strict";
--- public/js/src/amazon_controller.js
+++ public/js/src/amazon_controller.js
@@ -6,11 +6,25 @@ app.controller('AmazonController', ['$scope', '$timeout', 'Amazon',
     $scope.search = 'Ralph';
     $scope.category = 'Toys';
     $scope.products = [];
+    $scope.loading = false;
+    var doSearchTimeout = false;
 
     $scope.searchAmazon = function(){
       Amazon.search($scope.category, $scope.search, function(data){
         $scope.products = data.Item;
       });
     };
+
+    $scope.$watch('search', function(){
+      if(doSearchTimeout){
+        $timeout.cancel(doSearchTimeout);
+      }
+
+      $scope.page = 1;
+
+      doSearchTimeout = $timeout(function(){
+        $scope.searchAmazon();
+      }, 500);
+    });
 
 }]);
